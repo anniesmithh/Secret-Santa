991699371e7cc42044dd9016a0787a08a0ff2468
anniesmithh <anniecsmith54@gmail.com>
2025-11-28 12:50:40 +0000

    Add files via upload

---
+++ config.js
@@ -0,0 +1,21 @@
+// JSONBin.io Configuration
+// This is a free JSON storage service that allows sharing data across devices
+// Sign up at https://jsonbin.io to get your own API key and bin ID
+
+const CONFIG = {
+    // JSONBin.io credentials - configured and ready to use!
+    API_KEY: '$2a$10$6msYwW8MjOCYabakIytZL.sIBynImmS4Sg47522n5AMGOvOv6xDZO',
+    BIN_ID: '692999c0ae596e708f76678a',
+    API_URL: 'https://api.jsonbin.io/v3'
+};
+
+// Initial wishlist structure
+const INITIAL_WISHLISTS = {
+    'Annie': [],
+    'Katie': [],
+    'Andy': [],
+    'Michelle': [],
+    'Raoul': [],
+    'Joyce': [],
+    'Vernon': []
+};
